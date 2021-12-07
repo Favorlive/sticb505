09e5a169a877561291717c2606b43e7f59b1d705
SelimBenIsmail <selim.doms@gmail.com>
2021-12-07 23:02:45 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './component/auth/auth.component';
+import { DeclarationCampComponent } from './component/declaration-camp/declaration-camp.component';
+import { DemandeSubsideComponent } from './component/demande-subside/demande-subside.component';
+import { InscriptionUniteComponent } from './component/inscription-unite/inscription-unite.component';
 
 const routes: Routes = [
-
+{ path: 'InscriptionUnite', component: InscriptionUniteComponent},
+{ path: 'DeclarationCamp', component: DeclarationCampComponent},
+{ path: 'DemandeSubside', component: DemandeSubsideComponent},
+{ path: 'Authentification', component: AuthComponent}
   
 ];
 
