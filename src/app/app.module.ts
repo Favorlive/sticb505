fa589fbe82b338fd9f657b9bb449e047e8cd96ca
SelimBenIsmail <selim.doms@gmail.com>
2021-12-07 17:49:20 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
-import { HeadbarComponent } from './headbar/headbar.component';
-import { ProgressbarComponent } from './progressbar/progressbar.component';
-import { AuthComponent } from './auth/auth.component';
-import { InscriptionUniteComponent } from './inscription-unite/inscription-unite.component';
-import { DeclarationCampComponent } from './declaration-camp/declaration-camp.component';
-import { DemandeSubsideComponent } from './demande-subside/demande-subside.component';
+import { HeadbarComponent } from './component/headbar/headbar.component';
+import { ProgressbarComponent } from './component/progressbar/progressbar.component';
+import { AuthComponent } from './component/auth/auth.component';
+import { InscriptionUniteComponent } from './component/inscription-unite/inscription-unite.component';
+import { DeclarationCampComponent } from './component/declaration-camp/declaration-camp.component';
+import { DemandeSubsideComponent } from './component/demande-subside/demande-subside.component';
 
 @NgModule({
   declarations: [
